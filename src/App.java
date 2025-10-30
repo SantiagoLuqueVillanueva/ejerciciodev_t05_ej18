7a7a60ff621955aeed37ff14e7b0bb99df05b103
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-30 23:45:24 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num1 = Integer.parseInt(System.console().readLine("Introduzca un número entero: "));
+        int num2 = Integer.parseInt(System.console().readLine("Introduzca un número entero distinto al anterior: "));
+
+        for (int i = Math.min(num1,num2); i <= Math.max(num1,num2); i++) {
+            System.out.print(i + " ");
+            i = i + 6;
+        }
     }
 }
